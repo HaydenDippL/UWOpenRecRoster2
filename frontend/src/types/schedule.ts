3fcefbcc3cb6af1b28510e90a9f24c66cdfb2506
Hayden Dippel <hdippel@wisc.edu>
2025-04-12 13:34:54 -0500

    updated types

--- frontend/src/types/schedule.ts
+++ frontend/src/types/schedule.ts
@@ -1,5 +1,20 @@
-// Schedule response json format from backend
+import { DateTime } from "luxon";
 
+
+export enum Gym {
+    bakke = "bakke",
+    nick = "nick",
+}
+
+export enum Facility {
+    courts = "courts",
+    pool = "pool",
+    esports = "esports",
+    mount_mendota = "mount_mendota",
+    ice_rink = "ice_rink",
+}
+
+// Schedule response json format from backend
 export interface ScheduleResp {
     bakke: Facilities;
     nick: Facilities;
@@ -18,4 +33,25 @@ export interface Event {
     location: string;
     start: string;                  // "2025-04-08T13:30:00"
     end: string;                    // "2025-04-08T13:30:00"
+}
+
+// objects stored after processing
+export interface Schedules {
+    bakke: ProcessedFacilities;
+    nick: ProcessedFacilities;
+}
+
+export interface ProcessedFacilities {
+    courts: ProcessedEvent[];
+    pool: ProcessedEvent[];
+    esports: ProcessedEvent[];
+    mount_mendota: ProcessedEvent[];
+    ice_rink: ProcessedEvent[];
+}
+
+export interface ProcessedEvent {
+    name: string;
+    bucket: number;
+    start: DateTime;
+    end: DateTime;
 }
