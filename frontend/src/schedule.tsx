58eb5222ca9d2653f69b367a21e6dbd5feaa3c17
Hayden Dippel <hdippel@wisc.edu>
2025-05-01 14:50:11 -0500

    add times to schedule

--- frontend/src/schedule.tsx
+++ frontend/src/schedule.tsx
@@ -18,6 +18,11 @@ export default function Schedule({ gym, facility, date, focusedActivities }: Sch
         return <></>;
     }
 
+    const times = ["8:00 AM", "10:00 AM", "12:00 PM", "2:00 PM", "4:00 PM", "6:00 PM", "8:00 PM", "10:00 PM"];
+
+    const svg_width: number = 400;
+    const svg_height: number = 800;
+
     const schedule: ProcessedFacilities | null = schedules ? schedules[gym] : null;
     const events: ProcessedEvent[] | null = schedule ? schedule[facility] : null;
 
@@ -27,18 +32,18 @@ export default function Schedule({ gym, facility, date, focusedActivities }: Sch
     const num_buckets: number = (events || []).reduce((acc, curr) => {
         return Math.max(acc, curr.bucket);
     }, 0) + 1;
-    const width: string = `${100 / num_buckets}%`;
+    const rect_width: string = `${100 / num_buckets}%`;
 
     const focusMode: boolean = Object.values(focusedActivities).some(bool => bool);
 
-    return <div className="relative w-[400px] h-[800px]">
+    return <div className={`relative w-[${svg_width}px] h-[${svg_height}px] ml-24`}>
         {schedule == null && (
             <div className="absolute inset-0 w-full h-full skeleton" />
         )}
         <svg
-            className="absolute inset-0 w-full h-full"
-            width="400"
-            height="800"
+            className="absolute inset-0 w-full h-full overflow-visible"
+            width={svg_width}
+            height={svg_height}
         >
             {events && events.map((event, i) => {
                 const start_minute: number = event.start.diff(sixAm, "minutes").toObject().minutes || 0;
@@ -47,7 +52,7 @@ export default function Schedule({ gym, facility, date, focusedActivities }: Sch
                 // Use to group the pickleball and badminton open rec events into one for the grayscale filter...
                 let logicalEventName: string;
                 if (event.name.includes("Open Rec") && (event.name.includes("Badminton") || event.name.includes("Pickleball"))) {
-                    logicalEventName = "Open Rec Badminton/Pickleball";;
+                    logicalEventName = "Open Rec Badminton/Pickleball";
                 } else {
                     logicalEventName = event.name;
                 }
@@ -59,7 +64,7 @@ export default function Schedule({ gym, facility, date, focusedActivities }: Sch
                         key={i}
                         x={`${100 * event.bucket / 8}%`}
                         y={`${100 * start_minute / MINUTES_IN_SCHEDULE}%`}
-                        width={width}
+                        width={rect_width}
                         height={`${100 * (end_minute - start_minute) / MINUTES_IN_SCHEDULE}%`}
                         onClick={() => console.log({
                             start: event.start.toISO(),
@@ -72,15 +77,26 @@ export default function Schedule({ gym, facility, date, focusedActivities }: Sch
                     />
                 );
             })}
-            {Array.from({ length: 8 }).map((_, i) => {
-                return <rect 
-                    key={i}
-                    x="0%"
-                    y={`${(100 * (i + 1) / 9)}%`}
-                    width="100%"
-                    height="0.5%"
-                    fill="hsla(0, 1.80%, 11.20%, 0.35)"
-                />
+            {times.map((time, i) => {
+                const y: number = 100 * (i + 1) / 9
+                return <g key={i}>
+                    <rect
+                        x="0%"
+                        y={`${y}%`}
+                        width="100%"
+                        height="0.5%"
+                        fill="hsla(0, 1.80%, 11.20%, 0.35)"
+                    />
+                    <text
+                        x="-10"
+                        y={`${y}%`}
+                        dominantBaseline="central"
+                        textAnchor="end"
+                        className="text-sm fill-current"
+                    >
+                        { time }
+                    </text>
+                </g>
             })}
         </svg>
     </div>
